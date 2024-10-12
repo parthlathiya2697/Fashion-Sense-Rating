65af5d408c29e0bdcdc5b946197152eaa3d00f42
parth <plathiya2611@gmail.com>
2024-10-12 17:15:43 +0530

    load env in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+# Load environment variables from .env file
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
