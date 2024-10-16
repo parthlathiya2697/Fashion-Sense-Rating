d485177ee959eaf0b56b0b0fbc7c7ea9dce91dd6
parth <plathiya2611@gmail.com>
2024-10-16 15:54:53 +0530

    update action for pm2 bug

--- frontend/src/app/components/StyleUploader.tsx
+++ frontend/src/app/components/StyleUploader.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react'
 import Image from 'next/image'
-import RequestCountDisplay from './RequestCountDisplay'
 
 interface RequestCountDisplayProps {
   maxRequestCount: number | null;
